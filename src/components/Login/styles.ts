341cf1ad02d0666ad3ea741b4cc733c324e07e20
Nacho Gonzalez <nachogonzalez@Nachos-MacBook-Air.local>
2022-03-31 21:10:53 +0200

    cambios css

--- src/components/Login/styles.ts
+++ src/components/Login/styles.ts
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width:100%;
     height: 100vh;
-    margin:auto;
     display:flex;
+    margin: -8px;
+    padding-right: 16px;
 `
 
 export const Form = styled.form`
